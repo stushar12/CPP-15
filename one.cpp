f645c0be5b403699b88c401119c6964aa3efd6a5
Tushar Saxena <stushar345@gmail.com>
2020-09-28 13:35:07 +0530

    Add files via upload

---
+++ one.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+void check(string str1)
+{
+    stack<char> s;
+    for(int i=0;i<str1.length();i++)
+    {
+        if(str1[i] == '{' || str1[i] == '(' || str1[i] == '[')
+            s.push(str1[i]);
+        else
+        {
+            if(s.top()=='{' && str1[i]=='}')
+                s.pop();
+            else if(s.top()=='(' && str1[i]==')')
+                s.pop();
+            else if(s.top()=='[' && str1[i]==']')
+                s.pop();
+            else
+                break;
+        }
+    }
+if(s.empty()==true)
+{
+    cout<<"balanced"<<endl;
+}
+else
+{
+     cout<<"not balanced"<<endl;
+}
+}
+int main()
+{
+    int n;
+    cin>>n;
+    string str;
+    while(n)
+    {
+        cin>>str;
+        check(str);
+        n--;
+    }
+   
+}
